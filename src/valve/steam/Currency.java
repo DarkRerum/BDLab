a6502f29f87c81ff894238d0fb8e14876e246bc2
Nikita Sinyaev <sinyaev.ns@gmail.com>
2016-10-28 14:06:46 +0300

    Currency class

---
+++ src/valve/steam/Currency.java
@@ -0,0 +1,32 @@
+package valve.steam;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Nikita on 28.10.2016.
+ */
+public class Currency {
+    private long m_id;
+    private String m_name;
+    private float m_usdMult;
+
+    private Currency(long id, String name, float usdMult) {
+        m_id = id;
+        m_name = name;
+        m_usdMult = usdMult;
+    };
+
+    public static Currency getFromName(String name) throws SQLException {
+        String query = "SELECT * FROM currencies WHERE name = ?";
+
+        PreparedStatement ps = Steam.getInstance().getConnection().prepareStatement(query);
+        ps.setString(1, name);
+        ResultSet queryResult = ps.executeQuery();
+        queryResult.next();
+
+        return new Currency(queryResult.getLong(1), queryResult.getString(2), queryResult.getFloat(3));
+
+    }
+}
