7206d042019bbaeefa138d11cc2f5fdc90ea4c2a
rerum <t@t.t>
2016-12-12 22:45:41 +0300

    added redis lib and an example

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,19 @@ import valve.steam.*;
 import valve.util.PasswordExtractor;
 import valve.util.SteamCLI;
 
+import redis.clients.jedis.*;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		PasswordExtractor passwordExtractor = new PasswordExtractor();
 		
 		try {
+			Jedis jedis = new Jedis("localhost");
+			jedis.set("foo", "bar");
+			String value = jedis.get("foo");
+			System.err.println(value);
+			
 			Steam.getInstance().resetConnection("jdbc:oracle:thin:@localhost:1521:orbis",
 					passwordExtractor.getUsername(), passwordExtractor.getPassword());
 
